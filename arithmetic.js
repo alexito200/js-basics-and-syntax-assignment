dee6f2d41900621fab8dc8390b5579d12103adb2
alex <alexalarcon200@gmail.com>
2024-12-14 15:48:39 -0500

    this is my js basics and syntax assignment

---
+++ arithmetic.js
@@ -0,0 +1,71 @@
+// Javascript Operators
+// Task 1: Declare variables to store two numeric values for performing arithmetic operations.
+
+let x;
+let y;
+let z;
+let a;
+let b;
+let even;
+let odd;
+let positive;
+let negative;
+// Task 2: Assign sample numeric values to the variables declared in Task 1.
+
+// Arithmetic
+x = 5;
+y = 6;
+z = x + y;
+zz = x - y;
+zzz = x * y;
+zzzz = x / y;
+
+// Assignment
+x = 10;
+yy = x += y;
+yyy = x -= y;
+yyyy = x *= y;
+yyyyy = x /= y;
+
+// Comparison
+a = x == 10;
+aa = x === 20;
+aaa = x != 4;
+aaaa = x !== 3;
+aaaaa = x > y;
+aaaaaa = x < y;
+
+// Logical
+even = x % 2 === 0 && y % 2 === 0;
+odd = x % 2 !== 0 && y % 2 !== 0;
+positive = x > 0 && y > 0;
+negative = x < 0 && y < 0;
+
+// Task 3: Perform arithmetic operations on your numeric values using various operators and display the results.
+
+// Arithmetic
+console.log("Sum:", z);
+console.log("Difference:", zz);
+console.log("Product:", zzz);
+console.log("Quotient:", zzzz);
+
+// Assignment
+console.log(x);
+console.log(yy);
+console.log(yyy);
+console.log(yyyy);
+console.log(yyyyy);
+
+// Comparison
+console.log(a);
+console.log(aa);
+console.log(aaa);
+console.log(aaaa);
+console.log(aaaaa);
+console.log(aaaaaa);
+
+// Logical
+console.log(even);
+console.log(odd);
+console.log(positive);
+console.log(negative);
---
+++ data-types.js
@@ -0,0 +1,15 @@
+// Data Types
+// Task 1: Declare variables to represent student information such as name, age, grade, and attendance status.
+
+const studentName = 'Alex';
+let age;
+let grade;
+let attendanceStatus;
+
+age = 18;
+grade = '12th';
+attendanceStatus = 'True';
+
+// Task 3: Display the student information using console.log() statements.
+
+console.log(studentName, age, grade, attendanceStatus);
